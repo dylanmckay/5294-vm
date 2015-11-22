ac7099a61e6cfd82cda8206c26cca590f2f4d8cc
Dylan McKay <dylan.mckay@powershop.co.nz>
2015-11-23 12:39:46 +1300

    Add CPU core test

--- spec/vm/core_spec.rb
+++ spec/vm/core_spec.rb
@@ -1,9 +1,39 @@
 require_relative '../../lib/vm/core'
+require_relative '../../lib/vm/cli_bus'
 
 describe Core do
 
+  def integer(value)
+    Operand.integer(value)
+  end
+
+  def a
+    Operand.a
+  end
+
+  def in
+    Operand.in
+  end
+
+  def out
+    Operand.out
+  end
+
+  def add(value)
+    UnaryInstruction.new(:add, value)
+  end
+
   let(:core) {
-    Core.new()
+    Core.new([], instance_double(CliBus))
   }
+
+  context "binary instructions" do
+
+    context "add instruction" do
+      it "increments a value when adding by one" do
+        expect { core.execute(add(integer(1))) }.to change {core.a}.by(1)
+      end
+    end
+  end
 
 end

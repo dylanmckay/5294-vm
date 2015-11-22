4e37557a3fd67b6539d7dd3a32174c803b088980
Dylan McKay <dylan.mckay@powershop.co.nz>
2015-11-23 12:17:24 +1300

    Fix typo
    Cpu was renamed to Core

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,6 @@ SimpleCov.start
 
 require_relative "../lib/parser/parser"
 require_relative "../lib/vm/cli_bus"
-require_relative "../lib/vm/cpu"
+require_relative "../lib/vm/core"
 require_relative "../lib/vm/instruction"
 require_relative "../lib/vm/operand"

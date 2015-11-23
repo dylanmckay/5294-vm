21a892615af6da5a0fca1e53e321b8ffed721c68
Dylan McKay <dylan.mckay@powershop.co.nz>
2015-11-23 16:36:51 +1300

    Wrote more tests

---
+++ spec/vm/cli_bus_spec.rb
@@ -0,0 +1,21 @@
+
+require_relative '../../lib/vm/cli_bus'
+
+describe CliBus do
+
+  let(:bus) { CliBus.new }
+
+  # describe "#read_integer" do
+  #   it "calls 'gets'" do
+  #     expect(bus).to receive(:gets).and_return(2)
+  #     bus.read_integer
+  #   end
+  # end
+  #
+  # describe "#write_integer" do
+  #   it "calls 'puts'" do
+  #     expect(bus).to receive(:puts)
+  #     bus.write(integer(1))
+  #   end
+  # end
+end
--- spec/vm/dispatcher_spec.rb
+++ spec/vm/dispatcher_spec.rb
@@ -1,1 +1,33 @@
 require_relative '../../lib/vm/dispatcher.rb'
+require_relative '../../lib/vm/cli_bus'
+
+describe Dispatcher do
+
+  let(:programs) { [
+    [
+      UnaryInstruction.new(:add, Operand.integer(2)),
+      UnaryInstruction.new(:sub, Operand.integer(8)),
+      UnaryInstruction.new(:add, Operand.integer(0)),
+    ],
+
+    [
+      PlainInstruction.new(:swp),
+      JumpInstruction.new(:jmp, Operand.integer(6)),
+    ],
+  ]}
+
+  let(:dispatcher) {
+    Dispatcher.new(programs, bus: instance_double(CliBus))
+  }
+
+  describe "#running?" do
+    it "is is true when the dispatcher is created" do
+      expect(dispatcher.running?).to eq true
+    end
+
+    it "is false once all cores have finished" do
+      dispatcher.run
+      expect(dispatcher.running?).to eq false
+    end
+  end
+end
